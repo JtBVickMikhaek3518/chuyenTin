1ddcef2ea9a556b94e5eab0bda4ad1cb4df98865
JtB Vick Mikhaek <minhkhang.vophuoc@gmail.com>
2025-05-19 20:34:53 +0700

    0

--- 2026PTNKDraft/1PriLCM/main.cpp
+++ 2026PTNKDraft/1PriLCM/main.cpp
@@ -1,14 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int soLuongBoi(long long a, long long b, int c) {
-    int count = 0;
-
-    for (long long i = a; i < b; i++)
-        if (i % c == 0)
-            count++;
-
-    return count;
+int lcmNoFlow(int a, int b) {
+    return a * b / gcd(a, b);
 }
 
 int main() {
@@ -20,7 +14,23 @@ int main() {
     int x, y, z;
     cin >> a >> b >> x >> y >> z;
 
-    
+    int dx = b / x - (a - 1) / x;
+    int dy = b / y - (a - 1) / y;
+    int dz = b / z - (a - 1) / z;
+
+    int dxy = b / lcmNoFlow(x, y) - (a - 1) / lcmNoFlow(x, y);
+    int dyz = b / lcmNoFlow(y, z) - (a - 1) / lcmNoFlow(y, z);
+    int dxz = b / lcmNoFlow(x, z) - (a - 1) / lcmNoFlow(x, z);
+
+    int ox, oy, oz;
+    int dxyz = b / lcmNoFlow(lcmNoFlow(x, y), z) - (a - 1) / lcmNoFlow(lcmNoFlow(x, y), z);
+
+    ox = dx - dxy - dxz + dxyz;
+    oy = dy - dxy - dyz + dxyz;
+    oz = dz - dxz - dyz + dxyz;
+
+    int result = ox + oy + oz;
+    cout << result;
 
     return 0;
 }
